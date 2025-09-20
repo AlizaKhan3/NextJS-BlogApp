edeca53a18ea7f3430bc6c7b084737c52c614403
Aliza Khan <alizaadi2@gmail.com>
2025-09-20 21:29:26 +0500

    updated middleware -- added return statement

--- day-39/middleware.ts
+++ day-39/middleware.ts
@@ -19,7 +19,7 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // '/login'
+    '/login'
     // "/((?!_next/static|_next/image|favicon.ico).*)",
   ],
 }
